8945d6f5c13f7b523332a087889081522d7844d3
natandovale <natandovale496@gmail.com>
2022-06-05 15:25:39 -0300

    Adicionando contexto

--- PADROES-ARQUITETURAIS/NerdStore/src/NerdStore.Catalogo.Data/CatalogoContext.cs
+++ PADROES-ARQUITETURAIS/NerdStore/src/NerdStore.Catalogo.Data/CatalogoContext.cs
@@ -10,7 +10,6 @@ namespace NerdStore.Catalogo.Data
 {
     public class CatalogoContext : DbContext, IUnitOfWork
     {
-
         public CatalogoContext(DbContextOptions<CatalogoContext> options) :
             base(options) { }
 
@@ -20,16 +19,28 @@ namespace NerdStore.Catalogo.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
-                  e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
                 property.Relational().ColumnType = "varchar(100)";
 
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
         }
 
-        public Task<bool> Commit()
+        public  async Task<bool> Commit()
         {
-            throw new NotImplementedException();
-        }
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                }
 
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataCadastro").IsModified = false;
+                }
+            }
+
+            return await base.SaveChangesAsync() > 0;
+        }
     }
 }
--- PADROES-ARQUITETURAIS/NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
+++ PADROES-ARQUITETURAIS/NerdStore/src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -1,12 +1,16 @@
-﻿using NerdStore.Core.DomainObjects;
+﻿using System.Collections.Generic;
+using NerdStore.Core.DomainObjects;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class Categoria : Entity
     {
-        
+
         public string Nome { get; private set; }
-        public int Codigo { get; private set; }
+        public int? Codigo { get; private set; }
+        public ICollection<Produto>? Produtos { get; set; }
+
+        protected Categoria(){}
 
         public Categoria(string nome, int codigo) 
         {
